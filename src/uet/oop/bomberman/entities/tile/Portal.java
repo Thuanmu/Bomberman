1e6a2411e41658a9d57f2f74f732a2f19a2e8cd2
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2018-11-26 22:20:25 +0700

    Add files via upload

--- src/uet/oop/bomberman/entities/tile/Portal.java
+++ src/uet/oop/bomberman/entities/tile/Portal.java
@@ -1,17 +1,34 @@
 package uet.oop.bomberman.entities.tile;
 
+import uet.oop.bomberman.Board;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Portal extends Tile {
+	protected Board _board;
 
-	public Portal(int x, int y, Sprite sprite) {
+	public Portal(int x, int y, Sprite sprite, Board board) {
 		super(x, y, sprite);
+		_board = board;
 	}
 	
 	@Override
 	public boolean collide(Entity e) {
 		// TODO: xử lý khi Bomber đi vào
+		if(e instanceof Bomber) {
+
+			if(_board.detectNoEnemies() == false)
+				return false;
+
+			if(e.getXTile() == getX() && e.getYTile() == getY()) {
+				if(_board.detectNoEnemies())
+					_board.nextLevel();
+			}
+
+			return true;
+		}
+
 		return false;
 	}
 
